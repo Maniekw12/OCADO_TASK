b4a89a522b0894d969b3f3c74c2e8f3e6aa1fc72
Marian Wachala <maniekw12w@gmail.com>
2025-05-09 22:58:36 +0200

    add DataParser

---
+++ src/main/java/org/example/optimizer/PaymentOptimizationService.java
@@ -0,0 +1,15 @@
+package org.example.optimizer;
+
+import org.example.models.Order;
+import org.example.models.PaymentMethod;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class PaymentOptimizationService  {
+    private static final String POINTS_ID = "PUNKTY";
+    private static final int PARTIAL_POINTS_DISCOUNT = 10;
+
+
+
+}
